b81885621049ec583b164e87523ba5d317dd0a9f
prayag4 <prayag6547@gmail.com>
2025-09-05 07:42:27 +0530

    extending base page

--- cypress/support/pages/BasePage.js
+++ cypress/support/pages/BasePage.js
@@ -1,13 +1,79 @@
-export default class BasePage{
-    goto(url){
-        cy.visit(url)
-    }
-    clickElement(selector){
-        cy.get(selector).click()
+import 'cypress-iframe';
+import 'cypress-xpath';
+
+export default class BasePage {
+    goto(url) {
+        return cy.visit(url);
     }
 
-    findElementSubject(selector){
-        cy.get(selector)
+    findElement(selector) {
+        const trimmedSelector = selector.trim();
+        const isXPath = trimmedSelector.startsWith('//')
+            || trimmedSelector.startsWith('(')
+            || trimmedSelector.startsWith('/')
+            || trimmedSelector.startsWith('.//');
+        return isXPath ? cy.xpath(selector) : cy.get(selector);
     }
+
+    clickElement(selector) {
+        return this.findElement(selector).click();
+    }
+
+    typeinput(selector, text) {
+        return this.findElement(selector).clear().type(text);
+    }
+
+    waitForIframeToLoad(selector) {
+        return cy.frameLoaded(selector);
+    }
+
+    getIframeBody(selector) {
+        return cy.iframe(selector);
+    }
+
+    typeTextInIframe(iframeSelector, selector, text) {
+        return this.getIframeBody(iframeSelector).find(selector).clear().type(text);
+    }
+
+    selectDropdownElement(selector,value) {
+        return this.findElement(selector).select(value)
+    }
+
+    getAttribute(selector,attrName){
+        return this.findElement(selector).invoke('attr',attrName)
+    }
+
+    uploadFile(selector,filePath){
+        return this.findElement(selector).selectFile(filePath)
+    }
+
+    selectDate(day, month, year) {
+    this.findElement('.react-datepicker__year-dropdown-container').click();
+    this.findElement('.react-datepicker__year-option').contains(year).click();
+
+    this.findElement('.react-datepicker__month-dropdown-container').click();
+    this.findElement('.react-datepicker__month-option').contains(month).click();
+
+    const paddedDay = day.toString().padStart(2, '0');
+    const dateLocator = `div[class*="react-datepicker__day--0${paddedDay}"][aria-label*="${month} ${day}"]`;
+    
+    this.clickElement(dateLocator);
+  }
+
+    clickElementWithJs(selector) {
+    this.findElement(selector).then(($el) => {
+      $el[0].click();
+    });
+  }
+
+  getTextContent(selector){
+    return this.findElement(selector).invoke('text')
+  }
+
+  getAllFieldNamesInTable(selector) {
+    cy.get('table tr:last-child td:last-child').should('be.visible');
+    return cy.get(selector).then($els => Cypress._.map($els, 'innerText'));
+  }
+
 
 }
